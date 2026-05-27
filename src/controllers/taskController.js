df86d8d13c035ee0ec39db25d66153cf1a67e67a
jigyasha1234 <jigrani0000@gmail.com>
2026-05-27 23:07:37 +0530

    Initial backend setup

---
+++ src/controllers/taskController.js
@@ -0,0 +1,92 @@
+const Task = require("../models/Task");
+
+exports.createTask = async (req, res) => {
+  try {
+    const { title, description } = req.body;
+
+    const task = await Task.create({
+      title,
+      description,
+      user: req.user._id,
+    });
+
+    res.status(201).json(task);
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
+
+exports.getTasks = async (req, res) => {
+  try {
+    const tasks = await Task.find({
+      user: req.user._id,
+    });
+
+    res.json(tasks);
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
+
+exports.updateTask = async (req, res) => {
+  try {
+    const task = await Task.findById(req.params.id);
+
+    if (!task) {
+      return res.status(404).json({
+        message: "Task not found",
+      });
+    }
+
+    if (task.user.toString() !== req.user._id.toString()) {
+      return res.status(401).json({
+        message: "Unauthorized",
+      });
+    }
+
+    const updatedTask = await Task.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      {
+        new: true,
+      }
+    );
+
+    res.json(updatedTask);
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
+exports.deleteTask = async (req, res) => {
+  try {
+    const task = await Task.findById(req.params.id);
+
+    if (!task) {
+      return res.status(404).json({
+        message: "Task not found",
+      });
+    }
+
+    if (task.user.toString() !== req.user._id.toString()) {
+      return res.status(401).json({
+        message: "Unauthorized",
+      });
+    }
+
+    await task.deleteOne();
+
+    res.json({
+      message: "Task removed",
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
